cb1786c82cd88f00d17a5984b1bc3b4fc8da826b
akshai-m <mylapuramakshaikumar@gmail.com>
2025-06-25 22:43:52 +0530

    feat:: A new feature

---
+++ server/src/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken'
+export async function signup(req, res) {
+  const { email, password, fullName } = req.body;
+
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+      });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        message: "Password must be at least 6 characters long",
+      });
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({
+        message: "Invalid email format",
+      });
+    }
+
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({
+        message: "Email already exists",
+      });
+    }
+    
+
+    
+    res.status(201).json({
+      success: true,
+      user: newUser,
+    });
+  } catch (error) {
+    console.log(`Error in signup controller`, error)
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+}
+
+
+export function login(req, res) {
+  res.send("login page");
+}
+
+export function logout(req, res) {
+  res.send("logout page");
+}
