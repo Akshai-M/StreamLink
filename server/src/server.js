f74f61368488007c5e8ebbeaa2b39d0e5ee8ca7f
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-03 22:27:45 +0530

    refactor:: server

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import "dotenv/config"
 import authRouter from './routes/auth.router.js'
+import userRoute from './routes/user.route.js'
 import connectDB from './lib/db.js'
 import cookieParser from 'cookie-parser'
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use("/api/auth", authRouter)
+app.use('/api/users', userRoute)
 
 
 app.listen(PORT, () => {
