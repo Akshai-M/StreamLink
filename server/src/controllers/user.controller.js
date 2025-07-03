188fcc8920d535ba22739992b9213b0d6ad981e7
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-03 22:26:27 +0530

    refactor:: usercontroller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -5,7 +5,13 @@ export async function getRecommendedUser(req, res) {
     try {
         const currentUserId = req.user.id
         const currentUser = await req.user
-        
+        const rmdUsers = await User.find({
+            $and:[
+                {_id: {$ne: currentUserId}},
+                {$id: {$nin: currentUser.friends}},
+                { isOnboarded: true}
+            ]
+        })
     } catch (error) {
         console.error('Error in getRmd controller', error.message)
         res.status(500).json({ message: "Internal Server Error"})
