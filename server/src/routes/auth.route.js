6b385786c81251024f173e3f53cfa744183926fc
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-03 22:24:36 +0530

    refactor:: authroute

---
+++ server/src/routes/auth.route.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+import { login, logout, onborad, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
+const router = Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
+router.post('/onboarding', protectRoute, onborad)
+
+
+export default router;
