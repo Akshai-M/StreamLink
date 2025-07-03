4af09f0e8034e92099cd80385e5ab06253059442
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-03 22:24:00 +0530

    refactor:: userroute

---
+++ server/src/routes/user.route.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import { protectRoute } from '../middleware/auth.middleware.js'
+import { getMyFriends, getRecommendedUser } from '../controllers/user.controller.js'
+
+const router = express.Router()
+
+router.use(protectRoute)
+
+router.get('/',getRecommendedUser)
+router.get('/friends', getMyFriends)
+
+export default router
