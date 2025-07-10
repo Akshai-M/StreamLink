219edb2a317be09e8f2cbce7ddd0af33e9ee5b13
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-10 09:56:19 +0530

    refactor:: server

--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,25 @@
 import express from 'express'
 import "dotenv/config"
-import authRouter from './routes/auth.router.js'
+import authRouter from './routes/auth.route.js'
 import userRoute from './routes/user.route.js'
+import chatRoute from './routes/chat.route.js'
 import connectDB from './lib/db.js'
 import cookieParser from 'cookie-parser'
+import cors from "cors"
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credential: true
+}))
+
 app.use(express.json())
 app.use(cookieParser())
+
 app.use("/api/auth", authRouter)
 app.use('/api/users', userRoute)
+app.use("/api/chat", chatRoute)
 
 
 app.listen(PORT, () => {
