c517384c32b6c6d9bc4910553337ef5ea5d359f8
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-10 09:57:22 +0530

    refactor:: userroute

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { protectRoute } from '../middleware/auth.middleware.js'
-import { getMyFriends, getRecommendedUser } from '../controllers/user.controller.js'
+import { acceptFriendReq, getFriendReq, getMyFriends, getOutgoingReq, getRecommendedUser, sendFriendReq } from '../controllers/user.controller.js'
 
 const router = express.Router()
 
@@ -8,5 +8,8 @@ router.use(protectRoute)
 
 router.get('/',getRecommendedUser)
 router.get('/friends', getMyFriends)
+
+router.post("/friend-req/:id", sendFriendReq)
+router.post("/friend-req/:id/accept", acceptFriendReq)
 
 export default router
