0252284b9f397acdbdc682380b36a531d4512ba8
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-13 15:49:12 +0530

    refactor:: signuppage

--- client/src/pages/SignUpPage.jsx
+++ client/src/pages/SignUpPage.jsx
@@ -1,12 +1,25 @@
-import React from "react";
-import toast from "react-hot-toast";
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link, useNavigate } from "react-router";
+
+import useSignUp from "../hooks/useSignUp";
 
 function SignUpPage() {
+  const [signupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+  const { isPending, error, signupMutation } = useSignUp();
+const navigate = useNavigate();
+  const handleSignup = async (e) => {
+  e.preventDefault();
+  await signupMutation(signupData);
+  navigate("/login");
+};
+
   return (
-    <button onClick={() => toast.error("Hi! im created")}>
-      Create toast
-      
-    </button>
+    <></>
   );
 }
 
