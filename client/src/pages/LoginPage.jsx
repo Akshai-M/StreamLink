5bde33d0c44f46c10cf8b46cdb9ca1f75f66767d
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-14 18:21:56 +0530

    feat:: loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+
 
 function LoginPage() {
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  })
+
+  const queryClient = useQueryClient()
+  const {mutate: loginMutation, isPending, error} = useMutation({
+    mutationFn:login, 
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"]})
+  })
+  const handleLogin = (e) => {
+    e.preventDefault()
+    loginMutation(loginData)
+  }
   return (
     <div>
       LoginPage
