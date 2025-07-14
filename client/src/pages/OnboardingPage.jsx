29a09585a61da6e55ff673bdca42defec6a1a713
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-14 09:47:49 +0530

    refactor:: onboardingpage

--- client/src/pages/OnboardingPage.jsx
+++ client/src/pages/OnboardingPage.jsx
@@ -54,7 +54,20 @@ const OnboardingPage = () => {
           <form onSubmit={handleSubmit} className="space-y-6">
             {/* PROFILE PIC CONTAINER */}
             <div className="flex flex-col items-center justify-center space-y-4">
-             
+              {/* IMAGE PREVIEW */}
+              <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+                {formState.profilePic ? (
+                  <img
+                    src={formState.profilePic}
+                    alt="Profile Preview"
+                    className="w-full h-full object-cover"
+                  />
+                ) : (
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )}
+              </div>
 
               {/* Generate Random Avatar BTN */}
               <div className="flex items-center gap-2">
