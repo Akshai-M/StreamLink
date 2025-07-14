453c605f91e8be53cfe1e1c7d4458d4630b33df3
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-14 22:11:38 +0530

    refactor:: layout

--- client/src/components/Layout.jsx
+++ client/src/components/Layout.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import Sidebar from './Sidebar'
+import Sidebar from "./Sidebar";
+import Navbar from "./Navbar";
 
-function Layout({children, showSidebar=false}) {
+const Layout = ({ children, showSidebar = false }) => {
   return (
-    <div>
-      {showSidebar && <Sidebar/>}
-      <div>
-        <Navbar/>
-        <main className='flex overflow-y-auto'>
-          {children}
-        </main>
+    <div className="min-h-screen">
+      <div className="flex">
+        {showSidebar && <Sidebar />}
+
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+
+          <main className="flex-1 overflow-y-auto">{children}</main>
+        </div>
       </div>
     </div>
-  )
-}
-
-export default Layout
+  );
+};
+export default Layout;
