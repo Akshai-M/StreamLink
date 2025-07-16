66e421d86f6eafd8cfac9370a6c5912e29fdb3b5
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-16 17:00:25 +0530

    refactor:: stream

--- server/src/lib/stream.js
+++ server/src/lib/stream.js
@@ -1,31 +1,15 @@
-import { StreamChat } from "stream-chat"
-import "dotenv/config"
+import { StreamChat } from "stream-chat";
+import "dotenv/config";
 
-const apiKey = process.env.STREAM_KEY
-const apiSecret = process.env.STREAM_SECRET
+const apiKey = process.env.STREAM_KEY;
+const apiSecret = process.env.STREAM_SECRET;
 
-if (!apiKey || !apiSecret){
-    console.error("Invalid Api creds")
-}
 
-const streamClient = StreamChat.getInstance(apiKey, apiSecret)
-
-export const upsertStreamUser  = async (userData) => {
-try {
-    
-    await streamClient.upsertUsers([userData])
-    return userData
-} catch (error) {
+export const upsertStreamUser = async (userData) => {
+  try {
+    await streamClient.upsertUsers([userData]);
+    return userData;
+  } catch (error) {
     console.log(`Error upserting stream user`, error);
-    
-}
-}
-
-export const generateStreamToken = (userId) => {
-    try {
-        const userIdStr = userId.toString()
-        return streamClient.createToken(userIdStr)
-    } catch (error) {
-        console.log("Error generating stream token", error)
-    }
-}
+  }
+};
