52be5db6ec4d36466a2f5cc33b3080cdb3c9e3c8
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-16 17:00:32 +0530

    refactor:: stream

--- server/src/lib/stream.js
+++ server/src/lib/stream.js
@@ -4,6 +4,11 @@ import "dotenv/config";
 const apiKey = process.env.STREAM_KEY;
 const apiSecret = process.env.STREAM_SECRET;
 
+if (!apiKey || !apiSecret) {
+  console.error("Invalid Api creds");
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
 export const upsertStreamUser = async (userData) => {
   try {
