fafac5cdad753f2f7d617513ae107df70b2cfffc
akshai-m <mylapuramakshaikumar@gmail.com>
2025-07-19 09:42:03 +0530

    refactor:: usercontroller

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,23 +1,21 @@
-import User from "../models/User"
+import User from "../models/User.js";
+import FriendRequest from "../models/FriendRequest.js";
 
-export async function getRecommendedUser(req, res) {
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
 
-    try {
-        const currentUserId = req.user.id
-        const currentUser = await req.user
-        const rmdUsers = await User.find({
-            $and:[
-                {_id: {$ne: currentUserId}},
-                {$id: {$nin: currentUser.friends}},
-                { isOnboarded: true}
-            ]
-        })
-        res.status(200).json(rmdUsers)
-    } catch (error) {
-        console.error('Error in getRmd controller', error.message)
-        res.status(500).json({ message: "Internal Server Error"})
-    }
-
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },
+        { _id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json(recommendedUsers);
+  } catch (error) {
+    console.error("Error in getRecommendedUsers controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
-
-export async function getMyFriends(req, res) {}
