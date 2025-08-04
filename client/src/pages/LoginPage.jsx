02ac5a6eac99d1eda456a1488f1d6b0f4704c5be
akshai-m <mylapuramakshaikumar@gmail.com>
2025-08-04 17:31:45 +0530

    feat: loginpage

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,26 +1,56 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useState } from "react";
+import { ShipWheelIcon } from "lucide-react";
+import { Link } from "react-router";
+import useLogin from "../hooks/useLogin";
 
-
-function LoginPage() {
+const LoginPage = () => {
   const [loginData, setLoginData] = useState({
     email: "",
     password: "",
-  })
+  });
 
-  const queryClient = useQueryClient()
-  const {mutate: loginMutation, isPending, error} = useMutation({
-    mutationFn:login, 
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"]})
-  })
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: loginMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: login,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  // This is how we did it using our custom hook - optimized version
+  const { isPending, error, loginMutation } = useLogin();
+
   const handleLogin = (e) => {
-    e.preventDefault()
-    loginMutation(loginData)
-  }
-  return (
-    <div>
-      LoginPage
-    </div>
-  )
-}
+    e.preventDefault();
+    loginMutation(loginData);
+  };
 
-export default LoginPage
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="forest"
+    >
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-lg overflow-hidden">
+        {/* LOGIN FORM SECTION */}
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* LOGO */}
+         
+
+          {/* ERROR MESSAGE DISPLAY */}
+          
+
+          <div className="w-full">
+           
+          </div>
+        </div>
+
+        {/* IMAGE SECTION */}
+        
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
